26217881e355c09a04d2bc84596662cc8395b4ee
Alopez-7 <alejandro.lopezgzz7@gmail.com>
2022-02-11 04:59:34 -0600

    Total de ventas por producto

---
+++ classes.py
@@ -0,0 +1,18 @@
+from dataclasses import dataclass
+from mimetypes import init
+from typing import List
+
+@dataclass
+class ItemSales:
+    
+    """Class for keeping track of an item in inventory."""
+    productName: str =''
+    idProduct: str =""
+    sales: int=0
+
+    
+
+  
+        
+
+    
--- main.py
+++ main.py
@@ -1,4 +1,21 @@
+from hashlib import new
 from lifestore_file import lifestore_products, lifestore_sales
+from classes import ItemSales
+from pprint import pprint
 
 
-print(lifestore_products[0])
+
+sales =  [ItemSales()]
+sales.clear()
+i=0
+products_sales = {}
+for product in lifestore_products:
+    sales.append(ItemSales(product[1],product[0]))
+    for productsale in lifestore_sales:
+        if productsale[1] == sales[i].idProduct:
+            sales[i].sales +=1
+    i+=1
+
+sortedBySales = sorted(sales, key=lambda x: x.sales,reverse=True)
+ 
+pprint(sortedBySales)
