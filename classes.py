8d345b58ca3c803d051ad960ffce93728f06c3c7
Alopez-7 <alejandro.lopezgzz7@gmail.com>
2022-02-11 18:44:07 -0600

    Top 5 ventas y Busquedas

--- classes.py
+++ classes.py
@@ -1,6 +1,10 @@
 from dataclasses import dataclass
+from itertools import product
 from mimetypes import init
+from pickle import EMPTY_LIST
+from queue import Empty
 from typing import List
+from unicodedata import category
 
 @dataclass
 class ItemSales:
@@ -8,11 +12,15 @@ class ItemSales:
     """Class for keeping track of an item in inventory."""
     productName: str =''
     idProduct: str =""
+    category:str =""
     sales: int=0
 
-    
+
+@dataclass
+class TopSearches:
+    productName: str =''
+    idProduct: str =""
+    searches: int=0
+
 
   
-        
-
-    
--- main.py
+++ main.py
@@ -1,6 +1,9 @@
 from hashlib import new
-from lifestore_file import lifestore_products, lifestore_sales
-from classes import ItemSales
+from math import prod
+from typing import List
+from unicodedata import category
+from lifestore_file import lifestore_products, lifestore_sales,lifestore_searches
+from classes import ItemSales, TopSearches
 from pprint import pprint
 
 
@@ -8,14 +11,60 @@ from pprint import pprint
 sales =  [ItemSales()]
 sales.clear()
 i=0
-products_sales = {}
 for product in lifestore_products:
-    sales.append(ItemSales(product[1],product[0]))
+    sales.append(ItemSales(product[1],product[0],product[3]))
     for productsale in lifestore_sales:
         if productsale[1] == sales[i].idProduct:
             sales[i].sales +=1
     i+=1
 
-sortedBySales = sorted(sales, key=lambda x: x.sales,reverse=True)
+searches = [TopSearches()]
+searches.clear()
+
+i=0
+for items in sales:
+    searches.append(TopSearches(items.productName,items.idProduct))
+    for searchesItem in lifestore_searches:
+        if searchesItem[1] == searches[i].idProduct:
+            searches[i].searches += 1
+    i +=1
+
  
-pprint(sortedBySales)
+def getTopSearches(qty:int,productSearches:List[TopSearches]):
+    sortedBySearches = sorted(productSearches,key=lambda x:x.searches, reverse=True)
+    topSearches = sortedBySearches[0:qty]
+    for items in topSearches:
+         print(f'\t  Nombre: {items.productName} ID: {items.idProduct} Ventas: {items.searches} ')
+
+
+def getTopSales(qty:int,productSales:List[ItemSales]):
+    sortedBySales = sorted(productSales, key=lambda x: x.sales,reverse=True)
+    topSales = sortedBySales[0:qty]
+    for items in topSales:
+        print(f'\t  Nombre: {items.productName} ID: {items.idProduct} Ventas: {items.sales} ')
+#getTopSales(5,sales)
+
+
+
+#prodCategories['test'] = sales[0].productName
+
+
+def makeCategoriesList(productSales:List[ItemSales]):
+    prodCategories = {}
+    for item in productSales:
+        if item.category not in prodCategories.keys():
+           prodCategories[item.category]=[]
+        prodCategories[item.category].append(item.idProduct)
+    return prodCategories
+
+
+        
+def bestProductsByCat(products:dict):
+    for cat in products.keys():
+        for itemSales in sales:
+            ##if cat in itemSales
+                
+                print()
+
+
+bestProductsByCat(makeCategoriesList(sales))
